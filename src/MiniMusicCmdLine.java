423ac5f52f301da82bc647b4fbe68d8b8bac14aa
karandash-zametkin <55867822+karandash-zametkin@users.noreply.github.com>
2019-09-30 16:49:59 +0400

    release v2 (version with GUI and Listeners)

--- src/MiniMusicCmdLine.java
+++ src/MiniMusicCmdLine.java
@@ -1,47 +1,96 @@
 import javax.sound.midi.*;
+import javax.swing.*;
+import java.awt.*;
 
 public class MiniMusicCmdLine {
+
+    static JFrame f = new JFrame("Мой первый музыкальный клип.");
+    static MyDrawPanel mdp;
+
+    public static MidiEvent makeEvent (int comd, int chan, int one, int two,  int tick) {
+        MidiEvent midiEvent = null;
+        try {
+            ShortMessage shortMessage = new ShortMessage();
+            shortMessage.setMessage(comd, chan, one, two);
+            midiEvent = new MidiEvent(shortMessage,  tick);
+        } catch (Exception e) {
+            return midiEvent;
+        }
+        return midiEvent;
+    }
+
+    public void setUpGui(){
+        mdp = new MyDrawPanel();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setContentPane(mdp);
+        f.setBounds(30, 30, 300, 300);
+        f.setVisible(true);
+    }
+
     public static void main(String[] args) {
         MiniMusicCmdLine mini = new MiniMusicCmdLine();
-        if (args.length < 2) {                                  // Если забыли ввести аргументы в командной строке
-            System.out.println("Не забудьте ввести аргументы: инструмент и ноты!");
-        } else {
-            int intriment = Integer.parseInt(args[0]);          // Преобразуем троки в числа
-            int note = Integer.parseInt(args[1]);
-            mini.play(intriment, note);
-        }
+        mini.play();
     }
-    public void play(int instrument, int note) {                //Метод создает и проигрывает звук
+
+    public void play() {
+
+        setUpGui();
 
         try {
-            Sequencer player = MidiSystem.getSequencer();       // Запрашиваем синтезатор
-            player.open();                                      // Открываем его, так как он не открыт в начале
-            Sequence sequence = new Sequence(Sequence.PPQ, 4); // Создаем последовательноесть
-            Track track = sequence.createTrack();               // Создаем трек из последовательности
+            Sequencer player = MidiSystem.getSequencer();
+            player.open();
+            player.addControllerEventListener(mdp, new int[] {127});
+            Sequence sequence = new Sequence(Sequence.PPQ, 4);
+            Track track = sequence.createTrack();
 
-            MidiEvent event = null;
+            int r = 0;
+            for (int i = 0; i < 60; i += 4) {
+                r = (int) ((Math.random() * 50) + 1);
+                track.add(makeEvent(144, 1, r, 100, i));
 
-            ShortMessage first = new ShortMessage();            // Создаем сообщение
-            first.setMessage(192, 1, instrument, 0); // Заполняем сообщение значениями
-            MidiEvent changeInstrument = new MidiEvent(first, 1); // Создаем МИДИ инструкции
-            track.add(changeInstrument);                        // Добавляем МИДИ инструкции в трек
+                track.add(makeEvent(176, 1, 127, 0, i));
 
-            ShortMessage a = new ShortMessage();
-            a.setMessage(144, 1, note, 100);
-            MidiEvent noteOn = new MidiEvent(a, 1);
-            track.add(noteOn);
+                track.add(makeEvent(128, 1, r, 100, i + 2));
+            }
 
-            ShortMessage b = new ShortMessage();
-            b.setMessage(128, 1, note, 100);
-            MidiEvent noteOff = new MidiEvent(b, 16);
-            track.add(noteOff);
-
-            player.setSequence(sequence);               // Указываем проигрываемую последовательность в синтезаторе
-            player.start();                             // Запускаем синтезатор
+            player.setSequence(sequence);
+            player.setTempoInBPM(120);
+            player.start();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+
+    class MyDrawPanel extends JPanel implements ControllerEventListener {
+
+        boolean msg = false;
+
+        @Override
+        public void controlChange(ShortMessage event) {
+            msg = true;
+            repaint();
+        }
+
+        public void paintComponent (Graphics g) {
+            if (msg) {
+                Graphics2D g2d = (Graphics2D) g;
+
+                int red = (int) (Math.random() * 255);
+                int green = (int) (Math.random() * 255);
+                int blue = (int) (Math.random() * 255);
+
+                g.setColor(new Color(red, green, blue));
+
+                int height = (int) ((Math.random() * 120) + 10);
+                int width = (int) ((Math.random() * 120) + 10);
+                int x = (int) ((Math.random() * 40) + 10);
+                int y = (int) ((Math.random() * 40) + 10);
+
+                g.fillRect(x, y, height, width);
+                msg = false;
+            }
+        }
     }
 }
